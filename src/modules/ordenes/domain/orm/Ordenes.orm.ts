86ec523d56f2c90db6593e26ab5c66550fa8278a
Miguel Herrera <software@memcosas.com>
2024-02-23 12:31:18 -0500

    adding changes to orm

--- src/modules/ordenes/domain/orm/Ordenes.orm.ts
+++ src/modules/ordenes/domain/orm/Ordenes.orm.ts
@@ -11,6 +11,17 @@ import { fallasAccionesEntity } from "../entities/Fallas_Acciones.entity";
 import { fallasCausasEntity } from "../entities/Fallas_Causas.entity";
 import { fallasModosEntity } from "../entities/Fallas_Modos.entity";
 import { modosFallosEntity } from "../entities/Modos_Fallos.entity";
+import { serviciosEntity } from "../../../procesos_&_protocolos/domain/entities/Servicios.entity";
+import { SolicitudesEstadosEntity } from "../../../procesos_&_protocolos/domain/entities/SolicitudesEstados.entity";
+import { equipoEntity } from "../../../equipos/domain/entities/Equipo.entity";
+import { sedeEntity } from "../../../users/domain/entities/Sede.entity";
+import { clientEntity } from "../../../users/domain/entities/Client.entity";
+import { modeloEquipoEntity } from "../../../equipos/domain/entities/ModeloEquipo.entity";
+import { marcaEquipoEntity } from "../../../equipos/domain/entities/MarcasEquipos.entity";
+import { classDeviceEntity } from "../../../equipos/domain/entities/ClassDevice.entity";
+import { areaEquipoEntity } from "../../../equipos/domain/entities/AreaEquipo.entity";
+import { tipoEquipoEntity } from "../../../equipos/domain/entities/TipoEquipo.entity";
+import { visitasEstadosEntity } from "../../../visitas/domain/entities/Visitas_Estados.entity";
 // Import other necessary entities for relationships
 
 // CRUD
@@ -28,6 +39,18 @@ export const getAllOrdenes = async (page: number, limit: number): Promise<any[] 
     const fallasCausasModel = fallasCausasEntity(); 
     const fallasModosModel = fallasModosEntity();
     const modosFallosModel = modosFallosEntity();
+    const servicioModel = serviciosEntity();
+    const solicitudEstadoModel = SolicitudesEstadosEntity();
+    const equipoModel = equipoEntity();
+    const sedeModel = sedeEntity();
+    const clientModel = clientEntity();
+    const modeloEquiposModel = modeloEquipoEntity();
+    const marcaEquipoModel = marcaEquipoEntity();
+    const claseEquipoModel = classDeviceEntity();
+    const areaEquipoModel = areaEquipoEntity();
+    const tipoEquipoModel = tipoEquipoEntity();
+
+    const visitaEstadoModel = visitasEstadosEntity();
     // Import and define other necessary models for population
 
     const ordenes: IOrden[] = await ordenModel
@@ -38,6 +61,64 @@ export const getAllOrdenes = async (page: number, limit: number): Promise<any[] 
       .populate({
         path: 'id_solicitud_servicio',
         model: solicitudServicioModel, // Assuming the model name for SolicitudServicio
+        populate : [
+          {
+            path: 'id_creador',
+            model: userModel,
+            select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+          },
+          {
+            path: 'id_servicio',
+            model: servicioModel,
+          },
+          {
+            path: 'id_solicitud_estado',
+            model: solicitudEstadoModel,
+          },
+          {
+            path: 'id_equipo',
+            model: equipoModel,
+            populate :[
+              {
+                path: 'id_sede',
+                model: sedeModel,
+                select: '_id sede_nombre sede_address sede_telefono sede_email id_client',
+                populate: {
+                  path: 'id_client',
+                  model: clientModel,
+                }
+
+              },
+              {
+                path: 'modelo_equipos',
+                model: modeloEquiposModel,
+                populate: [
+                  {
+                    path: 'id_marca',
+                    model: marcaEquipoModel,
+                  },
+                  {
+                    path: 'id_clase',
+                    model: claseEquipoModel,
+                  }
+                ]
+              },
+              {
+                path: 'id_area',
+                model: areaEquipoModel,
+              },
+              {
+                path: 'id_tipo',
+                model: tipoEquipoModel,
+              }
+            ]
+          },
+          {
+            path: 'id_cambiador',
+            model: userModel,
+            select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+          }
+        ]
       })
       .populate({
         path: 'id_orden_estado',
@@ -46,6 +127,33 @@ export const getAllOrdenes = async (page: number, limit: number): Promise<any[] 
       .populate({
         path: 'ids_visitas',
         model: visitaModel,
+        populate: [
+          {
+            path: 'id_visita_estado',
+            model: visitaEstadoModel,
+          },
+          {
+            path: 'id_responsable',
+            model: userModel,
+            select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+          },
+          {
+            path: 'id_creador',
+            model: userModel,
+            select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+          },
+          {
+            path: 'id_aprobador',
+            model: userModel,
+            select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+          }
+          ,
+          {
+            path: 'id_cerrador',
+            model: userModel,
+            select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+          }
+        ]
       })
       .populate({
         path: 'ids_orden_sub_estados',
@@ -54,7 +162,6 @@ export const getAllOrdenes = async (page: number, limit: number): Promise<any[] 
       .populate({
         path: 'id_creador',
         model: userModel,
-        select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
       })
       .populate({
         path: 'id_cerrador',
@@ -105,21 +212,97 @@ export const getAllOrdenes = async (page: number, limit: number): Promise<any[] 
 
 export const getOrdenByID = async (id: string): Promise<IOrden | undefined> => {
   try {
+    // models for population
+    const userModel = userEntity();
     const ordenModel = ordenesEntity();
-    // const userModel = userEntity();
+    const visitaModel = visitasEntity();
+    const ordenSubEstadoModel = ordenesSubEstadosEntity();
     const solicitudServicioModel = solicitudesServiciosEntity();
     const ordenEstadoModel = ordenesEstadosEntity();
-    // Import and define other necessary models for population
-
+    const fallasAccionesModel = fallasAccionesEntity(); 
+    const fallasCausasModel = fallasCausasEntity(); 
+    const fallasModosModel = fallasModosEntity();
+    const modosFallosModel = modosFallosEntity();
+    const servicioModel = serviciosEntity();
+    const solicitudEstadoModel = SolicitudesEstadosEntity();
+    const equipoModel = equipoEntity();
+    
     return await ordenModel
       .findById(id)
       .populate({
         path: 'id_solicitud_servicio',
-        model: solicitudServicioModel,
+        model: solicitudServicioModel, // Assuming the model name for SolicitudServicio
+        populate : [
+          {
+            path: 'id_creador',
+            model: userModel,
+            select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+          },
+          {
+            path: 'id_servicio',
+            model: servicioModel,
+          },
+          {
+            path: 'id_solicitud_estado',
+            model: solicitudEstadoModel,
+          },
+          {
+            path: 'id_equipo',
+            model: equipoModel,
+          },
+          {
+            path: 'id_cambiador',
+            model: userModel,
+            select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+          }
+        ]
       })
       .populate({
         path: 'id_orden_estado',
         model: ordenEstadoModel,
+      })
+      .populate({
+        path: 'ids_visitas',
+        model: visitaModel,
+      })
+      .populate({
+        path: 'ids_orden_sub_estados',
+        model: ordenSubEstadoModel,
+      })
+      .populate({
+        path: 'id_creador',
+        model: userModel,
+        select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+      })
+      .populate({
+        path: 'id_cerrador',
+        model: userModel,
+        select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+      })
+      .populate({
+        path: 'ids_fallas_acciones',
+        model: fallasAccionesModel,
+      })
+      .populate({
+        path: 'ids_fallas_causas',
+        model: fallasCausasModel,
+      })
+      .populate({
+        path: 'ids_falla_modos',
+        model: fallasModosModel,
+      })
+      .populate({
+        path: 'modos_fallas_ids',
+        model: modosFallosModel,
+      })
+      .populate({
+        path: 'entrega.id_entrega',
+        model: userModel,
+        select: 'number username name cedula telefono email more_info roles type titulo reg_invima',
+      })
+      .populate({
+        path: 'modos_fallas_ids',
+        model: modosFallosModel,
       })
       // Populate other relationships similarly
       .exec();
